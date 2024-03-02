deb95b781be76afcd35ac45f1a2c322d77c418b4
EzZy001 <120203312+EzZy001@users.noreply.github.com>
2024-03-02 18:18:53 +0100

    update

--- index.php
+++ index.php
@@ -3,10 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PD Urmince</title>
 </head>
 <body>
     <h1>PD Urmince</h1>
-    <p>PD urmince</p>
+    <div class= "menu">
+        <div>
+            <a href="index.php">Domov</a>
+        </div>
+        <div>
+            <a href="index.php?stranka=kontakt">Kontakt</a>
+    </div>
 </body>
 </html>
