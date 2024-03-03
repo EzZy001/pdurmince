5f332bcc7fb4ae93b3b045a54c8a221cb9f667b3
EzZy001 <120203312+EzZy001@users.noreply.github.com>
2024-03-04 00:03:04 +0100

    update

---
+++ contact.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PD Urmince</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class= "menu">
+        <div><a href="index.php">Domov</a></div>
+        <div><a href="about.php">O nás</a></div>
+        <div><a href="contact.php">Kontakt</a>
+        <div><a href="mechanizacia.php">Mechanizácia</a>
+        <div><a href="vyroba.php">Rastlinná výroba</a></div>
+    </div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -4,14 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PD Urmince</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class= "menu">
-        <ul>
-            <li><a href="index.php">Domov</a></li>
-            <li><a href="about.php">O nás</a></li>
-            <li><a href="contact.php">Kontakt</a></li>
-        </ul>
+        <div class="item"><a href="index.php">Domov</a></div>
+        <div class="item"><a href="about.php">O nás</a></div>
+        <div class="item"><a href="contact.php">Kontakt</a>
+        <div class="item"><a href="mechanizacia.php">Mechanizácia</a>
+        <div class="item"><a href="vyroba.php">Rastlinná výroba</a></div>
     </div>
 </body>
 </html>
