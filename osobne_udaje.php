6ea000abf8dee95c2a1adb5af9c64b6540b5d283
EzZy001 <120203312+EzZy001@users.noreply.github.com>
2024-03-05 13:25:26 +0100

    update
    vela zmien

--- osobne_udaje.php
+++ osobne_udaje.php
@@ -1,11 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
 <header>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PD Urmince</title>
+    <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="styles/osobne_udaje.css">
 </header>
-
 <body>
-    <p>Zásady ochrany osobných údajov</p>
+    
+
+    <div class= "menu">
+        <div class="menu_item"><a href="index.php">Domov</a></div>
+        <div class="menu_item"><a href="about.php">O nás</a></div>
+        <div class="menu_item"><a href="contact.php">Kontakt</a></div>
+        <div class="menu_item"><a href="mechanizacia.php">Mechanizácia</a></div>
+        <div class="menu_item"><a href="vyroba.php">Rastlinná výroba</a></div>
+    </div>
+
+
+
 </body>
 
 <?php
     include("parts/footer.php");
 ?>
+</html>
--- parts/footer.php
+++ parts/footer.php
@@ -6,6 +6,8 @@
                     echo "Poľnohospodárske družstvo Urmince";
                     echo "<br>";
                     echo "<a href='osobne_udaje.php'>Zásady ochrany osobných údajov</a>";
+                    echo "<br>";
+                    echo "<a href='login.php' style='text-decoration:none'>Prihlásenie</a>";
                 ?>
             </div>
 
