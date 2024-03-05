16729ac8b872453e74d3fdc35af354627819a9a4
EzZy001 <120203312+EzZy001@users.noreply.github.com>
2024-03-05 13:54:28 +0100

    update

--- osobne_udaje.php
+++ osobne_udaje.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
-<header>
+<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PD Urmince</title>
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="styles/osobne_udaje.css">
-</header>
+</head>
 <body>
     
 
