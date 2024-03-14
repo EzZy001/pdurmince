82efbbac43f980dd9789ee8b62c13cc49f6d1977
EzZy001 <120203312+EzZy001@users.noreply.github.com>
2024-03-15 00:12:47 +0100

    Frontend edit
    Fresh start novy vizual (obrazky farby sa budu menit)

--- index.php
+++ index.php
@@ -4,24 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PD Urmince</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="styles/style.css">
 </head>
-<?php 
-    include "parts/header.php";
-?>
 <body>
-
-    <div class= "menu">
-        <div class="menu_item"><a href="index.php">Domov</a></div>
-        <div class="menu_item"><a href="about.php">O nás</a></div>
-        <div class="menu_item"><a href="contact.php">Kontakt</a></div>
-        <div class="menu_item"><a href="mechanizacia.php">Mechanizácia</a></div>
-        <div class="menu_item"><a href="vyroba.php">Rastlinná výroba</a></div>
-    </div>
-
+    <?php
+        include 'parts/header.php';
+    ?>
 </body>
-
-<?php
-    include "parts/footer.php";
-?>
 </html>
